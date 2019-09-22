6bd8d0a01d898631966ebb566cbcda0d4aa789c7
ZhangHongwei <zhang20132348@gmail.com>
2019-09-22 11:37:37 +0800

    update2019/9/22 增加分类管理的add功能

--- src/main/java/com/how2java/tmall/controller/CategoryController.java
+++ src/main/java/com/how2java/tmall/controller/CategoryController.java
@@ -2,12 +2,20 @@ package com.how2java.tmall.controller;
 
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.ImageUtil;
 import com.how2java.tmall.util.Page;
+import com.how2java.tmall.util.UploadedImageFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -24,6 +32,17 @@ public class CategoryController {
         model.addAttribute("cs", cs);
         model.addAttribute("page", page);
         return "admin/listCategory";
+    }
+    @RequestMapping("admin_category_add")
+    public String add(Category category, HttpSession httpSession, UploadedImageFile uploadedImageFile) throws IOException {
+        categoryService.add(category);
+        File imageFolder = new File(httpSession.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder, category.getId()+".jpg");
+        file.getParentFile().mkdirs();
+        uploadedImageFile.getImage().transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img,"jpg",file);
+        return "redirect:/admin_category_list";
     }
 
 }
